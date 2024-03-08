257a4237e8f52aa26e32592788daff6048b8d871
Shubeesh <shubeeth001@warren.k12.in.usm>
2024-03-08 08:01:06 -0500

    blahblah

--- 08_OOP/05_Methods/assignment.py
+++ 08_OOP/05_Methods/assignment.py
@@ -1,30 +1,36 @@
-class Doctor():        
-    def __init__(self, first_name = 'John', last_name = 'Smith'):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.__format_names()
-    
-    def __format_names(self):
-        self.first_name = self.first_name.title()
-        self.last_name = self.last_name.title()
-        
-    def introduce(self):
-        print('Hi, I am', self.first_name)
-        
-    def compare_name(self, name_to_compare):
-        if self.first_name == name_to_compare:
-            print('We have the same name!')
-        else:
-            print('Sorry, my name is different...')
-    
-    def get_first_last_name_together(self):
-        return self.first_name + ' ' + self.last_name
-    
+class __Rectangle:
+    def _init_(self, base, height) -> None:
+        self.__base = base
+        self.__height = height
+
+    def get_height(self) -> float:
+        return self.__height
+
+    def get_base(self) -> float:
+        return self.__base
+
+    def get_perimeter(self)-> float:
+        return 2 * self.__base + 2 * self.__height
+
+    def get_area(self) -> float:
+        return self.__base * self.height
+
+    def get_count(self) -> float:
+        return self.get_count
+
     def __str__(self):
-        return 'doctor=' + self.first_name + ' ' + self.last_name
-        
-doc_alex = Doctor('AleXandeR', 'SMith')
-doc_alex.introduce()
-doc_alex.compare_name('John')
-print(doc_alex.__dict__)
-print(Doctor.__dict__)
+        return "Rectangle of base" + self.__base + ", height", + self.__height 
+
+Rectangle1 = __Rectangle(5, 3)
+print("The base of ractangle one is", Rectangle1.get_base())
+print("The height of rectangle one is", Rectangle1.get_height())
+print("The area of rectangle one is", Rectangle1.get_area())
+print("The perimeter of rectangle one is", Rectangle1.get_perimeter())
+print("")
+Rectangle2 = __Rectangle(6, 8)
+print("The base of ractangle two is", Rectangle2.get_base())
+print("The height of rectangle two is", Rectangle2.get_height())
+print("The area of rectangle two is", Rectangle2.get_area())
+print("The perimeter of rectangle two is", Rectangle2.get_perimeter())
+print(__Rectangle.get_count())
+print("")
