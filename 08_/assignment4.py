eb16c6b6d4002f65a41e9588a5c6b427f7b76703
Shubeesh <shubeeth001@warren.k12.in.usm>
2024-03-08 07:44:11 -0500

    updateagain

---
+++ 08_/assignment4.py
@@ -0,0 +1,32 @@
+class __Rectangle:
+    def _init_(self, base, height) -> None:
+        self.__base = base
+        self.__height = height
+
+    def get_height(self) -> float:
+        return self.__height
+
+    def get_base(self) -> float:
+        return self.__base
+
+    def get_perimeter(self)-> float:
+        return 2 * self.__base + 2 * self.__height
+
+    def get_area(self) -> float:
+        return self.__base * self.height
+
+    def get_count(self) -> float:
+        return self.get_count
+
+Rectangle1 = __Rectangle
+print("The base of ractangle one is", Rectangle1.base())
+print("The height of rectangle one is", Rectangle1.height())
+print("The area of rectangle one is", Rectangle1.area())
+print("The perimeter of rectangle one is", Rectangle1.perimeter())
+
+Rectangle2 = __Rectangle
+print("The base of ractangle two is", Rectangle2.base())
+print("The height of rectangle two is", Rectangle2.height())
+print("The area of rectangle two is", Rectangle2.area())
+print("The perimeter of rectangle two is", Rectangle2.perimeter())
+
