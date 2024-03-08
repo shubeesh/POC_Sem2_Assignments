ba0cd21e8c45684b6ad3b12ca540f28fead25788
Shubeesh <shubeeth001@warren.k12.in.usm>
2024-03-08 07:54:11 -0500

    update4854812

--- 08_OOP/01_Intro/assignment.py
+++ 08_OOP/01_Intro/assignment.py
@@ -13,3 +13,63 @@ print("The area of triangle1 is", triangle1.area())
 #YOUDO: make another rightTriangle called triangle2
 #YOUDO:  and print its area.  
 
+# # class User:
+# #     def __init__(self):
+# #         self.nickname = 'sampleNickname'
+# #         self.city = 'sampleCity'
+
+# #     def introduce(self):
+# #         print('Hello, I am', self.nickname, 'and I live in', self.city)
+
+
+# # sample_user = User()
+# # sample_user.introduce()
+# # print(sample_user.nickname)
+# # print(sample_user.city)
+
+
+# # class User:
+# #     def __init__(self, nickname, city):
+# #         self.nickname = nickname
+# #         self.city = city
+
+# #     def introduce(self):
+# #         print('Hello, I am', self.nickname, 'and I live in', self.city)
+
+
+# # first_user = User('DarkKnight', 'Gotham')
+# # second_user = User('Superman', 'Metropolis')
+# # third_user = User('Martin', 'Boston')
+# # first_user.introduce()
+# # second_user.introduce()
+# # third_user.introduce()
+
+# class User:
+#     def __init__(self, nickname="SampleNickname", city="SampleCity"):
+#         self.nickname = nickname
+#         self.city = city
+
+#     def introduce(self):
+#         print('Hello, I am', self.nickname, 'and I live in', self.city)
+
+
+# first_user = User('DarkKnight', 'Gotham')
+# second_user = User('Superman', 'Metropolis')
+# third_user = User('Martin', 'Boston')
+# no_name_no_city_user = User()
+# no_city_user = User("Fred")
+# no_name_user = User(city="Chicago")
+
+
+# first_user.introduce()
+# second_user.introduce()
+# third_user.introduce()
+# no_name_no_city_user.introduce()
+# no_city_user.introduce()
+# no_name_user.introduce()
+
+x = 4 
+y = 5
+z = 3
+
+print(x == y or z)
--- 08_OOP/04_Class and Instance Variables/assignment.py
+++ 08_OOP/04_Class and Instance Variables/assignment.py
@@ -1,29 +1,31 @@
-class Rectangle:
-    
-    
-    
-    def __init__(self, base: float, height: float) -> None:
-        if(base < 0):
-            self.__base = 0
-        else:       
-            self.__base = base
-            
-        if(height < 0):
-            self.__height = 0
-        else:
-            self.__height = height
-        
+class __Rectangle:
+    def _init_(self, base, height) -> None:
+        self.__base = base
+        self.__height = height
+
     def get_height(self) -> float:
         return self.__height
-    
-    #YOUDO the get_base method
-    
-    def get_perimeter(self) -> float:
+
+    def get_base(self) -> float:
+        return self.__base
+
+    def get_perimeter(self)-> float:
         return 2 * self.__base + 2 * self.__height
-    
-    #Youdo get_area method
- 
- 
- 
-#YOUDO>  create two rectangles.  print their base, height, perimeter, and area
-#using only the methods not the fields/property/attributes
+
+    def get_area(self) -> float:
+        return self.__base * self.height
+
+    def get_count(self) -> float:
+        return self.get_count
+
+Rectangle1 = __Rectangle
+print("The base of ractangle one is", Rectangle1.base())
+print("The height of rectangle one is", Rectangle1.height())
+print("The area of rectangle one is", Rectangle1.area())
+print("The perimeter of rectangle one is", Rectangle1.perimeter())
+
+Rectangle2 = __Rectangle
+print("The base of ractangle two is", Rectangle2.base())
+print("The height of rectangle two is", Rectangle2.height())
+print("The area of rectangle two is", Rectangle2.area())
+print("The perimeter of rectangle two is", Rectangle2.perimeter())
