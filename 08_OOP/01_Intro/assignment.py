e0c5b913b821b4b3ff5b39c24a073465539bfd3a
Shubeesh <shubeeth001@warren.k12.in.usm>
2024-03-20 08:09:31 -0400

    krytiogn

--- 08_OOP/01_Intro/assignment.py
+++ 08_OOP/01_Intro/assignment.py
@@ -1,75 +1,15 @@
 class RightTriangle:
     def __init__(self, base, height):
         self.base = base
-        #YOUDO:  do the same for height
+
+    def __init__(self, base, height):
+        self.height = height
     
     def area(self):
-        #Youdo return the area which is 1/2 * base * height
-        pass #Remove this pass when finished
+        return 1/2 * self.base * self.height
 
 triangle1 = RightTriangle(3, 4)
+triangle2 = RightTriangle(5, 7)
 print("The area of triangle1 is", triangle1.area())
-
-#YOUDO: make another rightTriangle called triangle2
-#YOUDO:  and print its area.  
-
-# # class User:
-# #     def __init__(self):
-# #         self.nickname = 'sampleNickname'
-# #         self.city = 'sampleCity'
-
-# #     def introduce(self):
-# #         print('Hello, I am', self.nickname, 'and I live in', self.city)
-
-
-# # sample_user = User()
-# # sample_user.introduce()
-# # print(sample_user.nickname)
-# # print(sample_user.city)
-
-
-# # class User:
-# #     def __init__(self, nickname, city):
-# #         self.nickname = nickname
-# #         self.city = city
-
-# #     def introduce(self):
-# #         print('Hello, I am', self.nickname, 'and I live in', self.city)
-
-
-# # first_user = User('DarkKnight', 'Gotham')
-# # second_user = User('Superman', 'Metropolis')
-# # third_user = User('Martin', 'Boston')
-# # first_user.introduce()
-# # second_user.introduce()
-# # third_user.introduce()
-
-# class User:
-#     def __init__(self, nickname="SampleNickname", city="SampleCity"):
-#         self.nickname = nickname
-#         self.city = city
-
-#     def introduce(self):
-#         print('Hello, I am', self.nickname, 'and I live in', self.city)
-
-
-# first_user = User('DarkKnight', 'Gotham')
-# second_user = User('Superman', 'Metropolis')
-# third_user = User('Martin', 'Boston')
-# no_name_no_city_user = User()
-# no_city_user = User("Fred")
-# no_name_user = User(city="Chicago")
-
-
-# first_user.introduce()
-# second_user.introduce()
-# third_user.introduce()
-# no_name_no_city_user.introduce()
-# no_city_user.introduce()
-# no_name_user.introduce()
-
-x = 4 
-y = 5
-z = 3
-
-print(x == y or z)
+print("The area of triangle2 is", triangle2.area())
+print(triangle2.area())
